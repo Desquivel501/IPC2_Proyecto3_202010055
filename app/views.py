246f15ca48f1df74f7febbfeb5987eb444fb2f5e
Desquivel501 <3523114870101@ingenieria.usac.edu.gt>
2021-10-18 15:05:55 -0600

    added css

--- app/views.py
+++ app/views.py
@@ -4,7 +4,14 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("Ejemplo")
+    if request.method == 'POST':
+        if 'entrada' in request.POST:
+            entrada = request.POST['entrada']
+            print("Entrada: ", entrada)
+        if 'file' in request.POST:
+            file = request.POST['file']
+            print(file)
+    return render(request, 'app/base.html')
 
 def login(request):
     return HttpResponse("Login")
